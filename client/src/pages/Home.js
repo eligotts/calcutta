1af09c327da9b707ef0d3754411dbad4625944a7
eligotts <ergotts2002@yahoo.com>
2022-07-07 23:46:50 -0700

    started django

---
+++ client/src/pages/Home.js
@@ -0,0 +1,17 @@
+import React from 'react'
+//import { Link } from "react-router-dom";
+
+function Home() {
+
+    return (
+      <>
+        <div>
+            <p>Hello World</p>
+            <input type = "text" />
+            <button>Add</button>
+        </div>
+      </>
+    )
+  }
+  
+  export default Home
